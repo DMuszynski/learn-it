8284c063ed9db0c56ad740488bbb2f61e765e1c3
Damian Muszyński <d.muszynski112@gmail.com>
2023-03-28 18:02:57 +0200

    Initial commit

---
+++ account-service/src/main/java/pl/dmuszynski/accountservice/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.dmuszynski.accountservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class AccountServiceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
